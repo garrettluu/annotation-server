f5819cc75153266bf61ebc4a38ec0e87babe308e
Garrett Luu <garrettluu131@gmail.com>
2020-12-11 17:02:41 -0800

    Implemented prototype for saving annotations and measurements

--- app.js
+++ app.js
@@ -1,20 +1,34 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+require('dotenv').config();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const { MongoClient } = require('mongodb');
+const app = express();
+app.use(cors());
 
-var app = express();
+(async () => {
+    const client = new MongoClient(process.env.DB_URL);
+    await client.connect();
+    const db = client.db('pointcloud');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+    // const indexRouter = require('./routes/index');
+    // const usersRouter = require('./routes/users');
+    const pointcloudRouter = require('./routes/pointclouds')(db);
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
+    app.use(logger('dev'));
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false }));
+    app.use(cookieParser());
+    app.use(express.static(path.join(__dirname, 'public')));
+
+    // app.use('/', indexRouter);
+    // app.use('/users', usersRouter);
+    app.use('/pointclouds', pointcloudRouter);
+
+})();
 module.exports = app;
+
---
+++ routes/pointclouds.js
@@ -0,0 +1,26 @@
+module.exports = (db) => {
+    const express = require('express');
+    const router = express.Router();
+    const annotationCollection = db.collection('annotations');
+
+    /* GET all annotation groups for a pointcloud */
+    router.get('/:name', async (req, res, next) => {
+       res.send(await annotationCollection.find({name: req.params.name}).toArray());
+    });
+
+    /* POST new annotations in pointcloud */
+    router.post('/', async (req, res) => {
+        const newAnnotation = await annotationCollection.insertOne({name: req.body.name}, req.body);
+        res.status(200).json(newAnnotation);
+
+    });
+
+    /* PUT (update) annotations */
+    router.put('/', async (req, res) => {
+        const updatedAnnotation = await annotationCollection.replaceOne({name: req.body.name, group_name: req.body.group_name}, req.body);
+        res.status(200).json(updatedAnnotation);
+    });
+
+    return router;
+}
+
